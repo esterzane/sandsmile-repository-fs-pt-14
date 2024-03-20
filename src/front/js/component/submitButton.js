8bde422776aed7d04a85fb2aea21200ec4f0b4b0
Zane [Estere] Gruntmane <zaneestere@gmail.com>
2024-03-20 17:01:26 +0000

    new background

--- src/front/js/component/submitButton.js
+++ src/front/js/component/submitButton.js
@@ -5,21 +5,17 @@ export const SubmitButton = () => {
     const { store, actions } = useContext(Context)
     const [active, setActive] = useState(true)
     const [pending, setPending] = useState(false);
-  
+
 
     const handleSubmit = async () => {
 
         try {
 
-            if (store.manual_start_time && store.manual_finish_time &&  store.location && store.liters){
-                actions.submit_manual_data();
-                
-            } 
-            if  (store.start_time && store.finish_time &&  store.location && store.liters){
+            if (store.start_time && store.finish_time && store.location && store.liters) {
                 actions.submitData();
             }
-            
-           
+
+
         } catch (error) {
             console.error("An error occurred while submitting data: ", error);
         } finally {
@@ -29,9 +25,8 @@ export const SubmitButton = () => {
 
     return (
         <button type="button" className="user-location-button btn btn-success btn-sm" disabled={!active} onClick={handleSubmit}>
-            SUBMIT 
+            SUBMIT
         </button>
     );
-   
-}; 
- 
+
+};
